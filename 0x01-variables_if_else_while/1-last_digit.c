8343529ec7314f2f7d27a29a743e4c708e49bb72
Youssef Wagih <youssefwagih212@gmail.com>
2023-08-12 08:35:19 -0700

    Fixed mixed declarations warning in task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,12 +12,12 @@
 
 int main(void)
 {
-	int n;
+	int n, ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int ld  = n % 10;
+	ld  = n % 10;
 
 	if (ld > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
