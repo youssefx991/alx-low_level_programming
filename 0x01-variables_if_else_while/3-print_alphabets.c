8d03dba9e22921743f7f93c91934f9b5f09f7417
Youssef Wagih <youssefwagih212@gmail.com>
2023-08-12 08:47:09 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A program to print lowercase alaphapet
+ *
+ * Return: Alway 0 (success)
+*/
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+}
