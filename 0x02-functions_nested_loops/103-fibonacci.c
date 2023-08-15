36b5fbb5821b0e6a04bc433eeb6909f5f382d5c2
Youssef Wagih <youssefwagih212@gmail.com>
2023-08-15 14:41:54 -0700

    sum of even fib

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 fibonnacci
+ *
+ * Description: fibonnacic
+ *
+ * Return: 0 success
+*/
+
+int main(void)
+{
+	unsigned  long f = 1, s = 2, nxt, i, sum = 0;
+
+	for (i = 3; i <= 50; i++)
+	{
+		nxt = f + s;
+		f = s;
+		s = nxt;
+
+		if (nxt >= 4000000)
+			break;
+		if (nxt % 2 == 0)
+			sum += nxt;
+
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}
