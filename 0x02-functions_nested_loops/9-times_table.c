df34d0bd2d94a6f605db582d035e2c5198f5c775
Youssef Wagih <youssefwagih212@gmail.com>
2023-08-15 12:53:04 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,23 +12,18 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
+		_putchar('0');
 		for (j = 0; j <= 9; j++)
 		{
-			if (i * j >= 10)
-			{
-				_putchar(((i * j) / 10) + '0');
-				_putchar(((i * j) % 10) + '0');
-			}
-			else
-			{
-				_putchar((i * j) + '0');
-			}
+			_putchar(',');
+			_putchar(' ');
 
-			if (j != 9)
-			{
-				_putchar(',');
+			if ((i * j) <= 9)
 				_putchar(' ');
-			}
+			else
+				_putchar(((i * j) / 10) + '0');
+
+			_putchar(((i * j) % 10) + '0');
 		}
 		_putchar('\n');
 	}
