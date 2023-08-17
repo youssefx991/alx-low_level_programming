abdb9162e750962015cb04549c88673b26f6dbc2
Youssef Wagih <youssefwagih212@gmail.com>
2023-08-17 08:33:25 -0700

    prime_factor - testing with wrong result

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: find largest prime factor of a given number
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int i, largest = 2, curr, num = 612852475143, n = 782848;
+
+	while (num % 2 == 0)
+		num /= 2;
+
+	while (num % 3 == 0)
+		num /= 3;
+
+	for (i = 5; i <= n; i++)
+	{
+		if (num % i == 0)
+		{
+			curr = i;
+
+			while (num % i == 0)
+				num /= i;
+		}
+
+		if (curr >= largest)
+			largest = curr;
+	}
+
+	printf("%ld\n", largest);
+
+	return (0);
+}
