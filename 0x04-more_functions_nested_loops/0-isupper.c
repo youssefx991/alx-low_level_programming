a9110112375db1db154b3d42a8b83b394eb7f257
Youssef Wagih <youssefwagih212@gmail.com>
2023-08-17 06:43:55 -0700

    _isupper(int c)

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if char is uppercase
+ * @c: character to be checked
+ *
+ * Return: uppercase 1 lowercase 0.
+*/
+
+int _isupper(int c)
+{
+	return ((c >= 'A' && c <= 'Z'));
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -34,3 +34,4 @@ void print_remaining_days(int month, int day, int year);
 
 int convert_day(int month, int day);
 
+int _isupper(int c);
