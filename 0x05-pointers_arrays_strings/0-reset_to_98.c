97b17f98703957b306ec60e048130f7a329e65f1
Youssef Wagih <youssefwagih212@gmail.com>
2023-08-21 05:27:36 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets int value to 98
+ * @n: pointer pointing to integer
+ *
+ * Return: void.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -55,3 +55,5 @@ void print_square(int size);
 void print_triangle(int size);
 
 void print_number(int n);
+
+void reset_to_98(int *n);
