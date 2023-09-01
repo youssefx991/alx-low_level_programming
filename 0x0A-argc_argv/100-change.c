8c913c284e95aecfb37cdb986ba24576d50e497e
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-01 05:28:28 -0700

    find the change of money in arguments

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: size of argv
+ * @argv: array of arguments
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		int amount, twentyFives, tens, fives, twos, ones;
+
+
+		amount = atoi(argv[1]);
+
+		twentyFives = amount / 25;
+		amount = amount % 25;
+
+		tens = amount / 10;
+		amount = amount % 10;
+
+		fives = amount / 5;
+		amount = amount % 5;
+
+		twos = amount / 2;
+		amount = amount % 2;
+
+		ones = amount;
+
+		printf("%d\n", twentyFives + tens + fives + twos + ones);
+
+	}
+	else
+	{
+		printf("Error\n");
+	}
+
+	return (0);
+}
