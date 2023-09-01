b22f32581b7725ab8e4dc0bc7a6c7d7f5c5b20f0
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-01 04:37:55 -0700

    number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: size of argv
+ * @argv: array of arguments
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
