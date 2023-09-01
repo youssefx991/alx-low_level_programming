dd4ff5a890f1e6923338f0c56e8798194f8fd2c1
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-01 04:41:35 -0700

    print args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: size of argc
+ * @argv: array of arguments
+ *
+ * Retunr 0;
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
