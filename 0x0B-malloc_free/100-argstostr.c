d81aa4f410b058d8bf1d9c8d8dea7ed1b57f6596
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-04 14:18:31 -0700

    argstostr - concatenates all args given to a string in heap

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * argstostr - concatenates all args
+ * @ac: number of args
+ * @av: array of args
+ *
+ * Return: concatenated args
+*/
+
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int size, i, j, k;
+
+	size = 0;
+	k = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		size += strlen(av[0]) + 1;
+	ptr = malloc(sizeof(char) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+		{
+			ptr[k] = av[i][j];
+		}
+		ptr[k] = '\n';
+		k++;
+	}
+
+	return (ptr);
+}
