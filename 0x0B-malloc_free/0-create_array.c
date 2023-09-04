03cc86d8622a7ada11d57631828c0a07f2332d1c
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-04 12:46:14 -0700

    create_array - handled malloc output

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,11 +13,10 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *ptr;
-
-	if (size == 0)
-		return (NULL);
-
 	ptr = malloc(sizeof(char) * size);
+
+	if (size == 0 || ptr == 0)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
