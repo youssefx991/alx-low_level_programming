4fb84c05562be2fad3805d830377712fd8f80228
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-07 05:44:41 -0700

    _calloc - same as calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,8 +10,9 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
-	char *ptr;
+	char *tmp;
+	void *ptr;
+	unsigned int n;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,8 +22,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
+	n = sizeof(int) * nmemb;
+	tmp = ptr;
+
+	while (n--)
+		*tmp++ = 0;
 
 	return (ptr);
 }
