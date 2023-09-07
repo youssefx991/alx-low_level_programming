21cc7d50f70c8024c474a31a6dfa126174958482
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-07 06:10:16 -0700

    calloc - used cahr pointer instead of int

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,43 +1,29 @@
 #include "main.h"
 
 /**
- * set - sets all bytes to a value
- * @ptr: pointer
- * @c: char to be setted to
- * @n: number of bytes
- *
- * Return: pointer to the changed pointer
-*/
-char *set(char *ptr, char c, unsigned int n)
-{
-	char *tmp = ptr;
-
-	while (n--)
-		*ptr++ = c;
-
-	return (tmp);
-}
-
-/**
  * _calloc - allocates memory for array
  * @nmemb: no of elements
  * @size: size of each element
  *
  * Return: Nothing
 */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	unsigned int i, n;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * nmemb);
+	n = size * nmemb;
+	ptr = malloc(n);
 
 	if (ptr == NULL)
 		return (NULL);
 
-	set(ptr, 0, sizeof(int) * nmemb);
+	for (i = 0; i < n; i++)
+		ptr[i] = 0;
 
 	return (ptr);
 }
