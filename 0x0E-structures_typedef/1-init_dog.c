b594ecacc137145e770b122ee45593865c3acfc4
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-11 09:05:05 -0700

    init_dog - Added function to initialize dog strucutre

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes dog struct
+ * @d: dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ *
+ * Return: void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
