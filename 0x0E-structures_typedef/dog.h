ce38166fefd3579c8f0dc494bd8f33565cc76ca3
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-11 08:58:24 -0700

    dog.h - creates a struct of dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * description: dog representation structure
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* _DOG_H */
