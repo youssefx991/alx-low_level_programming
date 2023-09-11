3c3157a213e13fb087a3c61def8778b9b2e0e7bb
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-11 08:38:15 -0700

    2-main.c - prints name of file compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: prints name of compilation file
+ *
+ * Return: 0 for success
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
