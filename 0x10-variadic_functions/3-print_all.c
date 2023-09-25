2899ca5cd898a12b8b0a2a807c4560391a4e9504
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-25 06:54:18 -0700

    0x13 - Initial Commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints everything
+ * @format: type of parameter
+ *
+ * Return: Nothing
+*/
+
+void print_all(const char * const format, ...)
+{
+
+}
