85bfaa64350a6802d0c87c814dc474a9bc81ec10
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-25 13:15:06 -0700

    free_listint2 - frees list of int

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees list of int
+ * @head: pointer to head
+ *
+ * Retunrn: nothing
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *node = *head;
+	listint_t *tmp;
+
+	if (!head)
+		return;
+
+	while (*head)
+	{
+		tmp = *head;
+		node = node->next;
+		free(tmp);
+	}
+}
