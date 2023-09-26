35faf7fa78bac248eed068cfeaec7a569c93f854
Youssef Wagih <youssefwagih212@gmail.com>
2023-09-26 01:25:10 -0700

    delete_nodeint_at_index - nullity check

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	listint_t *deleted_node, *tmp;
 
-	if (!head)
+	if (!head || !*head)
 		return (-1);
 
 	i = 0;
