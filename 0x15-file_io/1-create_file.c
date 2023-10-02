a9890a0f38cb9b65347781084bdf59bdc87b17ab
Youssef Wagih <youssefwagih212@gmail.com>
2023-10-02 09:16:21 -0700

    create_file - added function like strlen

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * length - length of string
+ * @s: given string
+ *
+ * Return: length of string
+*/
+int length(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
  * create_file - creates a file
  * @filename: name of file to create
  * @text_content: contents of created file
@@ -9,20 +26,16 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
-	ssize_t chars;
+	int fd;
+	ssize_t chars, n = length(text_content);
 
 	if (!filename || !text_content)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	for (i = 0; text_content[i] != '\0'; i++)
-	{
-	}
-
-	chars = write(fd, text_content, i);
+	chars = write(fd, text_content, n);
 	close(fd);
 
-	return (chars == i ? 1 : -1);
+	return (chars == n ? 1 : -1);
 }
