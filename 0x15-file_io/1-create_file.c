a6fe722a849fbadb96aba8a62ee4ba6cd33abfcb
Youssef Wagih <youssefwagih212@gmail.com>
2023-10-02 09:20:38 -0700

    create_file - modified initial check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t chars = 0, n = length(text_content);
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
